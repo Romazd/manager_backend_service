b761fa96826d921484db89b68d09ba9800274b7b
Marco Sandoval <segundo.marco@gmail.com>
2024-03-08 14:33:31 -0600

    test hello

--- app/controllers/repo_manager_controller.rb
+++ app/controllers/repo_manager_controller.rb
@@ -13,9 +13,11 @@ class RepoManagerController < ApplicationController
     response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
       http.request(request)
     end
-  
+    Rails.logger.info("Response: #{response.body}")
     repos = JSON.parse(response.body) if response.is_a?(Net::HTTPSuccess)
     repos.map { |repo| repo['name'] }
+    Rails.logger.info("Repos: #{repos}")
+    "hello"
   rescue => e
     puts "Error: #{e.message}"
     []
